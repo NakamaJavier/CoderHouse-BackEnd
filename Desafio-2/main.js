d68273f717cddbbf25a4599a2b7ab82f3759ec84
JavierNakama <nakamajavier@hotmail.com>
2023-08-07 22:00:55 -0300

    se modifica para trabajar con extension de archivo .json y se agregan mas productos a la lista

--- Desafio-2/main.js
+++ Desafio-2/main.js
@@ -38,7 +38,7 @@ class ProductManager {
             product.id = this.idGenerator()
             this.products.push(product)
             const productsJson = JSON.stringify(this.products)
-            await fs.writeFile(`${this.path}products.txt`, productsJson)
+            await fs.writeFile(`${this.path}products.json`, productsJson)
             console.log("\n\nSe agrego el producto:", product, "\nCon exito a la lista" );
         }else
         console.log("\n\nError: No se agrego el producto",product, " porque el codigo: ",product.code, "ingresado ya existe en la lista");
@@ -48,7 +48,7 @@ class ProductManager {
 
     async getProducts(){
         try{
-        let contenido = await fs.readFile(`${this.path}products.txt`,"utf-8")
+        let contenido = await fs.readFile(`${this.path}products.json`,"utf-8")
         const contenidoParseado = JSON.parse(contenido)
         this.products = contenidoParseado
         } catch(error){
@@ -70,7 +70,7 @@ class ProductManager {
             const updatedProducts = this.products.filter((product) => product.id !== id);
             this.products = updatedProducts
             const updatedProductsJson = JSON.stringify(updatedProducts)
-            await fs.writeFile(`${this.path}products.txt`, updatedProductsJson)
+            await fs.writeFile(`${this.path}products.json`, updatedProductsJson)
             console.log("\n\nSe borro correctamente el producto. La lista de productos actual es:", this.products);
         }else
         console.log("\n\nNo se pudo eliminar porque el producto con esa ID no existe");
@@ -83,7 +83,7 @@ class ProductManager {
             newProduct.id = id;
             this.products[index] = newProduct;
             const productsJson = JSON.stringify(this.products)
-            await fs.writeFile(`${this.path}products.txt`, productsJson)
+            await fs.writeFile(`${this.path}products.json`, productsJson)
             console.log("\n\nSe actualizo el el producto:",this.products[index]);
             }
         }else
@@ -101,9 +101,22 @@ const prod2 = new Product("Dulce de leche Serenisima","Dulce de leche tipo colon
 console.log("\n\nEl segundo producto es :\n",prod2);
 const prod3 = new Product("Termo Lumilagro",12000,null,"www.fdhfdhjdf.com/afsag.jpg",4431,5)
 console.log("\n\nEl tercer producto es :\n",prod3);
-const prod4 = new Product("Termo Lumilagro","termo doble capa de acero inoxidable",12000,"www.fdhfdhjdf.com/afsag.jpg",6456,5)
+const prod4 = new Product("Termo Lumilagro","Termo doble capa de acero inoxidable",12000,"www.fdhfdhjdf.com/afsag.jpg",6456,5)
 console.log("\n\nEl cuarto producto es :\n",prod4);
-
+const prod6 = new Product("Pate Swift","Pate de foie",500,"www.asfdsafdsa.com/aghdshg.jpg",13679,250)
+console.log("\n\nEl primer producto es:\n",prod6);
+const prod7 = new Product("Pepas Futuro","Galletita dulce con relleno de membrillo",1000,"www.asfdsafdsa.com/qwtsd.jpg",8647,100)
+console.log("\n\nEl primer producto es:\n",prod7);
+const prod8 = new Product("Yerba Mate Playadito","Yerba mate",1100,"www.asfdsafdsa.com/sdfgh.jpg",8520,200)
+console.log("\n\nEl primer producto es:\n",prod8);
+const prod9 = new Product("Termo Lumilagro","Termo doble capa de acero inoxidable",12000,"www.fdhfdhjdf.com/ertre.jpg",2739,5)
+console.log("\n\nEl primer producto es:\n",prod9);
+const prod10 = new Product("Yogurt Yogurisimo","Yogurt entero firme de vainilla",500,"www.asfdsafdsa.com/asdgsd.jpg",2046,100)
+console.log("\n\nEl primer producto es:\n",prod10);
+const prod11 = new Product("Queso cremoso Paulina","Queso cremoso",3120,"www.asfdsafdsa.com/dfshgdf.jpg",3214,15)
+console.log("\n\nEl primer producto es:\n",prod11);
+const prod12 = new Product("Leche Serenisima","Leche descremada",420,"www.asfdsafdsa.com/qwhgcjd.jpg",5436,120)
+console.log("\n\nEl primer producto es:\n",prod12);
 
 //Se crea una instancia de la clase ProductManager
 const Products = new ProductManager("./")
@@ -113,6 +126,13 @@ await Products.addProduct(prod1)
 await Products.addProduct(prod2)
 await Products.addProduct(prod3)
 await Products.addProduct(prod4)
+await Products.addProduct(prod6)
+await Products.addProduct(prod7)
+await Products.addProduct(prod8)
+await Products.addProduct(prod9)
+await Products.addProduct(prod10)
+await Products.addProduct(prod11)
+await Products.addProduct(prod12)
 
 //Se muestra la funcionalidad de getProducts()
 await Products.getProducts()
@@ -129,5 +149,7 @@ const prod5 = new Product("avena quaker","avena instantanea",750,"www.asfdsafdsa
 await Products.updateProduct(1,prod5)
 
 //Se prueba la funcion feleteProduct con un ID valido y otro invalido
-await Products.deleteProduct(1)
-await Products.deleteProduct(3)
+//await Products.deleteProduct(1)
+//await Products.deleteProduct(3)
+await Products.getProducts()
+console.log("\n\nImpresion de la lista de preductos del getProducts:\n",Products.products);
